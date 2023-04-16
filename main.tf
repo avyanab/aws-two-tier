3ec3abc3e9b8e2c1db0a82af3b96116c5771a711
avyanab <avyanab@gmail.com>
2023-04-16 15:24:43 +0000

    added ssh sg

--- main.tf
+++ main.tf
@@ -141,6 +141,14 @@ resource "aws_nat_gateway" "tt-nat-gw" {
 resource "aws_security_group" "tf-tt-sg" {
   name   = "tf-tt-sg"
   vpc_id = aws_vpc.tt-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
